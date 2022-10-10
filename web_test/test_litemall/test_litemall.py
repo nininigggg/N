30775411bfe7f19b74447389fe8109f28348236b
nininigggg <nigengen@sina.cn>
2022-10-10 20:28:58 +0800

    test_litemall2

--- web_test/test_litemall/test_litemall.py
+++ web_test/test_litemall/test_litemall.py
@@ -14,6 +14,17 @@ class TestRecord(Base):
         self.driver.find_element(By.NAME, "password").clear()
         self.driver.find_element(By.NAME, "password").send_keys("manage123")
         self.driver.find_element(By.CSS_SELECTOR, ".el-button--primary").click()
-        time.sleep(10)
-
-
+        # time.sleep(10)
+        self.driver.maximize_window()
+        time.sleep(1)
+        self.driver.find_element(By.XPATH, "//*[text()='商场管理']").click()
+        self.driver.find_element(By.XPATH, "//*[text()='商品类目']").click()
+        self.driver.find_element(By.XPATH, "//*[text()='添加']").click()
+        self.driver.find_element(By.CSS_SELECTOR, '.el-input__inner').send_keys("新增商品测试")
+        button = self.driver.find_element(By.CSS_SELECTOR, '.dialog-footer .el-button--primary')
+        # 添加显示等待位置
+        time.sleep(3)
+        button.click()
+        time.sleep(1)
+        res = self.driver.find_element(By.XPATH, "//*[text()='新增商品测试']")
+        assert res != []
